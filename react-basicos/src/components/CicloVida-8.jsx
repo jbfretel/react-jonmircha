aa996f5f5199b0459c9b0560ac4e4e61c3f6a39c
JB <jluisbarrios787@gmail.com>
2022-05-07 09:06:30 -0500

    CicloVida

--- react-basicos/src/components/CicloVida-8.jsx
+++ react-basicos/src/components/CicloVida-8.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 
+//componente Clock creado para utilizar como ejemplo el componentWillunmount
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillUnmount() {
+    console.log(3, "WillUnmount - El componente ha sido eliminado del DOM");
+  }
+
+  render() {
+    return <h4>{this.props.hour}</h4>;
+  }
+}
+
 export default class CicloVida extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hour: new Date().toLocaleTimeString(),
+      visualize: false,
     };
 
     console.log(0, "'constructor', el componete aun no se encuentra en el DOM");
     this.timer = null;
   }
 
+  //============CICLO DE VIDA=============
+  componentDidMount() {
+    console.log(1, "DidMount - El componente ya esta montado en el DOM");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(
+      2,
+      "DidUpdate - El estado o las props del componente han cambiado"
+    );
+    console.log(prevProps);
+    console.log(prevState);
+  }
+
+  //======================================
+
   //metodo TICK
   tick = () => {
-    this.timer = setTimeout(() => {
+    this.timer = setInterval(() => {
       this.setState({ hour: new Date().toLocaleTimeString() });
     }, 1000);
   };
@@ -21,9 +52,11 @@ export default class CicloVida extends Component {
   //EVENTOS:
   handleStart = () => {
     this.tick();
+    this.setState({ visualize: true });
   };
   handleStop = () => {
     clearInterval(this.timer);
+    this.setState({ visualize: false });
   };
 
   render() {
@@ -31,7 +64,8 @@ export default class CicloVida extends Component {
     return (
       <>
         <h3>Ciclo de Vida</h3>
-        <h4>{this.state.hour}</h4>
+        {/* <h4>{this.state.hour}</h4> */}
+        {this.state.visualize && <Clock hour={this.state.hour} />}
         <button onClick={this.handleStart}>Start</button>
         <button onClick={this.handleStop}>Stop</button>
       </>
